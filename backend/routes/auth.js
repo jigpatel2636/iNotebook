4bb5dadff513874df8ab8a0bd87c7f17012487e8
Jigpatel2636 <jigpatel2636@gmail.com>
2022-08-18 23:02:50 -0400

    login_endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -41,7 +41,40 @@ router.post('/createuser', [body('email', 'Enter a valide email').isEmail(),
     res.json({auth_token})
     } catch(error) {
         console.log(error.message)
-        res.status(500).send("some error has occured")
+        res.status(500).send("Internal server error has occured")
+    }
+})
+
+// Authenticate User - Login
+router.post('/login', [body('email', 'Enter a valide email').isEmail(),
+                            body('password', 'pasword can\'t be blank').exists()],
+    async (req,res)=>{
+    
+    // if there are error, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({error:"Please try to login with correct credential"})
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please try to login with correct credential"})
+        }
+        const data  =  {
+            user:{
+                id: user.id
+            }
+        }
+        const auth_token = jwt.sign(data, JWT_SECRET)
+        res.send({auth_token})
+    } catch(error) {
+        console.log(error.message)
+        res.status(500).send("Internal server error has occured")
     }
 })
 
