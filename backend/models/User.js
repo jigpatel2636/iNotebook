60416cddbab56f715f61b4b42301d546e0cf83ea
Jigpatel2636 <jigpatel2636@gmail.com>
2022-08-18 22:41:16 -0400

    JWT

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,17 +9,18 @@ const UserSchema = new Schema({
     },
     email:{
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
-
     },
     date : {
         type: Date,
         default: Date.now
     }
 });
-
-module.exports = mongoose.model('user', UserSchema)
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,47 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const user = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Jigu@jigu"
 
 //create User using: POST "api/auth". Doesn't require auth
-router.post('/', (req,res)=>{
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/createuser', [body('email', 'Enter a valide email').isEmail(),
+                  body('name', 'Enter a valid name').isLength({ min: 3 }), 
+                  body('password').isLength({ min: 5 })], 
+    async (req,res)=>{
+          // if there are error, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //check if user with email exists 
+    try {
+    let user = await User.findOne({email:req.body.email});
+    if(user) {
+        return res.status(400).json({error: "User with this email already exists."})
+    }
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password, salt);
+    // create new user 
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass
+      })
+    const data  =  {
+        user:{
+            id: user.id
+        }
+    }
+    const auth_token = jwt.sign(data, JWT_SECRET)
+    res.json({auth_token})
+    } catch(error) {
+        console.log(error.message)
+        res.status(500).send("some error has occured")
+    }
 })
 
 module.exports = router
